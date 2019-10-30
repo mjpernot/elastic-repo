49e63e0a4cea7a08b610f9dce1ff6a58cd1e83c4
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-30 12:51:02 +0000

    Update create_repo.py

--- test/unit/elastic_db_repo/create_repo.py
+++ test/unit/elastic_db_repo/create_repo.py
@@ -9,7 +9,6 @@
         test/unit/elastic_db_repo/create_repo.py
 
     Arguments:
-        None
 
 """
 
@@ -33,7 +32,6 @@ import elastic_db_repo
 import lib.gen_libs as gen_libs
 import version
 
-# Version
 __version__ = version.__version__
 
 
@@ -42,10 +40,6 @@ class UnitTest(unittest.TestCase):
     """Class:  UnitTest
 
     Description:  Class which is a representation of a unit testing.
-
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:  None
 
     Methods:
         setUp -> Unit testing initilization.
@@ -65,7 +59,6 @@ class UnitTest(unittest.TestCase):
         Description:  Initialization for unit testing.
 
         Arguments:
-            None
 
         """
 
@@ -74,10 +67,6 @@ class UnitTest(unittest.TestCase):
             """Class:  ElasticSearchRepo
 
             Description:  Class representation of the ElasticSearchRepo class.
-
-            Super-Class:  object
-
-            Sub-Classes:  None
 
             Methods:
                 __init__ -> Initialize configuration environment.
@@ -92,7 +81,6 @@ class UnitTest(unittest.TestCase):
                 Description:  Initialization instance of the class.
 
                 Arguments:
-                        None
 
                 """
 
@@ -132,7 +120,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test err_flag is set to True.
 
         Arguments:
-            None
 
         """
 
@@ -149,7 +136,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test err_flag is set to False.
 
         Arguments:
-            None
 
         """
 
@@ -163,7 +149,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test repo name is not in list.
 
         Arguments:
-            None
 
         """
 
@@ -177,7 +162,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test repo name is in list.
 
         Arguments:
-            None
 
         """
 
@@ -194,7 +178,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test repo name is passed as an argument.
 
         Arguments:
-            None
 
         """
 
@@ -212,7 +195,6 @@ class UnitTest(unittest.TestCase):
         Description:  Test repo name is not passed as an argument.
 
         Arguments:
-            None
 
         """
 
