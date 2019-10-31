921d35974f735e18dc2fa56657893c8ec42d8019
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-31 09:22:57 +0000

    Update disk_usage.py

--- test/integration/elastic_db_repo/disk_usage.py
+++ test/integration/elastic_db_repo/disk_usage.py
@@ -68,14 +68,14 @@ class UnitTest(unittest.TestCase):
         self.repo_name = "TEST_INTR_REPO"
         self.repo_dir = os.path.join(self.cfg.base_repo_dir, self.repo_name)
 
-        self.ER = elastic_class.ElasticSearchRepo(self.cfg.host, self.cfg.port)
+        self.er = elastic_class.ElasticSearchRepo(self.cfg.host, self.cfg.port)
 
-        if self.ER.repo_dict:
+        if self.er.repo_dict:
             print("ERROR: Test environment not clean - repositories exist.")
             self.skipTest("Pre-conditions not met.")
 
         else:
-            _, _ = self.ER.create_repo(repo_name=self.repo_name,
+            _, _ = self.er.create_repo(repo_name=self.repo_name,
                                        repo_dir=self.repo_dir)
 
     def test_disk_usage(self):
@@ -97,7 +97,7 @@ class UnitTest(unittest.TestCase):
                 break
 
         with gen_libs.no_std_out():
-            self.assertFalse(elastic_db_repo.disk_usage(self.ER))
+            self.assertFalse(elastic_db_repo.disk_usage(self.er))
 
     def tearDown(self):
 
@@ -109,7 +109,7 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        err_flag, msg = self.ER.delete_repo(self.repo_name)
+        err_flag, msg = self.er.delete_repo(self.repo_name)
 
         if err_flag:
             print("Error: Failed to remove repository '%s'"
