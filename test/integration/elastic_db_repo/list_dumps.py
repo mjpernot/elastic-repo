d07eea06c861d4fb2674e060c1192f44158cd29d
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-10-31 09:23:55 +0000

    Update list_dumps.py

--- test/integration/elastic_db_repo/list_dumps.py
+++ test/integration/elastic_db_repo/list_dumps.py
@@ -69,14 +69,14 @@ class UnitTest(unittest.TestCase):
         self.repo_name = "TEST_INTR_REPO"
         self.repo_dir = os.path.join(self.cfg.base_repo_dir, self.repo_name)
 
-        self.ER = elastic_class.ElasticSearchRepo(self.cfg.host, self.cfg.port)
+        self.er = elastic_class.ElasticSearchRepo(self.cfg.host, self.cfg.port)
 
-        if self.ER.repo_dict:
+        if self.er.repo_dict:
             print("ERROR: Test environment not clean - repositories exist.")
             self.skipTest("Pre-conditions not met.")
 
         else:
-            _, _ = self.ER.create_repo(self.repo_name, self.repo_dir)
+            _, _ = self.er.create_repo(self.repo_name, self.repo_dir)
 
     def test_listdumps_none(self):
 
@@ -89,7 +89,7 @@ class UnitTest(unittest.TestCase):
         """
 
         with gen_libs.no_std_out():
-            self.assertFalse(elastic_db_repo.list_dumps(self.ER))
+            self.assertFalse(elastic_db_repo.list_dumps(self.er))
 
     def test_repo_dict(self):
 
@@ -101,9 +101,9 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        ES = elastic_class.ElasticSearchDump(self.cfg.host,
+        es = elastic_class.ElasticSearchDump(self.cfg.host,
                                              repo=self.repo_name)
-        err_flag, msg = ES.dump_db()
+        err_flag, msg = es.dump_db()
 
         if err_flag:
             print("Error detected for dump in repository: %s"
@@ -113,7 +113,7 @@ class UnitTest(unittest.TestCase):
 
         else:
             with gen_libs.no_std_out():
-                self.assertFalse(elastic_db_repo.list_dumps(self.ER))
+                self.assertFalse(elastic_db_repo.list_dumps(self.er))
 
     def test_repo_class_attr(self):
 
@@ -125,9 +125,9 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        ES = elastic_class.ElasticSearchDump(self.cfg.host,
+        es = elastic_class.ElasticSearchDump(self.cfg.host,
                                              repo=self.repo_name)
-        err_flag, msg = ES.dump_db()
+        err_flag, msg = es.dump_db()
 
         if err_flag:
             print("Error detected for dump in repository: %s"
@@ -136,10 +136,10 @@ class UnitTest(unittest.TestCase):
             self.skipTest("Dump failed")
 
         else:
-            self.ER.repo = self.repo_name
+            self.er.repo = self.repo_name
 
             with gen_libs.no_std_out():
-                self.assertFalse(elastic_db_repo.list_dumps(self.ER))
+                self.assertFalse(elastic_db_repo.list_dumps(self.er))
 
     def tearDown(self):
 
@@ -151,7 +151,7 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        err_flag, msg = self.ER.delete_repo(self.repo_name)
+        err_flag, msg = self.er.delete_repo(self.repo_name)
 
         if err_flag:
             print("Error: Failed to remove repository '%s'"
