ee1e0e0bba38ebe5b9372720b3475a7e8140f8ee
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-04-25 16:26:22 +0100

    Merge of mod/111c1 into mod/111

--- test/blackbox/elastic_db_repo/blackbox_test.py
+++ test/blackbox/elastic_db_repo/blackbox_test.py
@@ -3,10 +3,10 @@
 
 """Program:  blackbox_test.py
 
-    Description:  Blackbox testing of elastic_db_repo.py program.
+    Description:  Blackbox testing of elastic_db_dump.py program.
 
     Usage:
-        test/blackbox/elastic_db_repo/blackbox_test.py
+        test/blackbox/elastic_db_dump/blackbox_test.py
 
     Arguments:
 
@@ -46,14 +46,14 @@ def load_cfg(args_array):
     return gen_libs.load_module(args_array["-c"], args_array["-d"])
 
 
-def chk_create_repo(els, repo_name):
+def chk_create_repo(elr, repo_name):
 
     """Function:  chk_create_repo
 
     Description:  Check for the existence of a repository.
 
     Arguments:
-        (input) els -> Elasticsearch class instance.
+        (input) elr -> Elasticsearch class instance.
         (input) repo_name -> Name of repository.
         (output) status -> True|False - Status of check.
 
@@ -61,7 +61,7 @@ def chk_create_repo(els, repo_name):
 
     status = True
 
-    if repo_name not in els.repo_dict:
+    if repo_name not in elr.repo_dict:
         status = False
 
     return status
@@ -81,17 +81,19 @@ def create_es_instance(cfg, instance, repo_name=None):
 
     """
 
-    return instance(cfg.host, cfg.port, repo=repo_name)
+    return instance(
+        cfg.host, port=cfg.port, repo=repo_name, user=cfg.user,
+        japd=cfg.japd, ca_cert=cfg.ssl_client_ca, scheme=cfg.scheme)
 
 
-def remove_repo(els, repo_name, dump_loc):
+def remove_repo(elr, repo_name, dump_loc):
 
     """Function:  remove_repo
 
     Description:  Remove repository and cleanup directory.
 
     Arguments:
-        (input) els -> ElasticSearchRepo class instance.
+        (input) elr -> ElasticSearchRepo class instance.
         (input) repo_name -> Name of repository being removed.
         (input) dump_loc -> Location of repository.
         (output) status -> True|False - Status of repository removal.
@@ -100,7 +102,7 @@ def remove_repo(els, repo_name, dump_loc):
 
     status = True
 
-    err_flag, msg = els.delete_repo(repo_name=repo_name)
+    err_flag, msg = elr.delete_repo(repo_name=repo_name)
 
     if err_flag:
         status = False
@@ -122,67 +124,77 @@ def main():
 
     Variables:
         opt_val_list -> contains options which require values.
-        base_dir -> Relative directory path to blackbox testing directory.
-        test_path -> Current directory path concatencated with base_dir.
-        config_path -> Directory path to blackbox config directory.
 
     Arguments:
 
     """
 
-    prt_success = "\n\tTest Successful"
-    prt_failure = "\n\tTest Failure"
+    prt_good = "\n\tTest Successful"
+    prt_fail = "\n\tTest Failure"
+
     cmdline = gen_libs.get_inst(sys)
-    opt_val_list = ["-c", "-d", "-n", "-C", "-R", "-S", "-T"]
+    opt_val_list = ["-c", "-d", "-C", "-R", "-D", "-P"]
     args_array = arg_parser.arg_parse2(cmdline.argv, opt_val_list)
     cfg = load_cfg(args_array)
 
     if "-C" in args_array:
-        els = create_es_instance(cfg, elastic_class.ElasticSearchDump,
-                                 args_array["-C"])
         elr = create_es_instance(cfg, elastic_class.ElasticSearchRepo,
                                  args_array["-C"])
+        elr.connect()
 
         if chk_create_repo(elr, args_array["-C"]):
-            print(prt_success)
+            print(prt_good)
 
         else:
-            print(prt_failure)
+            print(prt_fail)
 
-        _ = remove_repo(elr, args_array["-C"], els.dump_loc)
+        _ = remove_repo(elr, args_array["-C"], args_array["-P"])
 
     elif "-R" in args_array:
-        els = create_es_instance(cfg, elastic_class.ElasticSearchDump,
-                                 args_array["-R"])
         elr = create_es_instance(cfg, elastic_class.ElasticSearchRepo,
                                  args_array["-R"])
-        _ = remove_repo(elr, args_array["-R"], els.dump_loc)
+        elr.connect()
+        _ = remove_repo(elr, args_array["-R"], args_array["-P"])
 
-    elif "-T" in args_array:
-        els = create_es_instance(cfg, elastic_class.ElasticSearchDump,
-                                 args_array["-T"])
-        els.dump_name = args_array["-n"]
-        els.dump_db()
-
-    elif "-S" in args_array:
-        els = create_es_instance(cfg, elastic_class.ElasticSearchDump,
-                                 args_array["-r"])
-
-        if args_array["-S"] in els.dump_list:
-            print(prt_failure)
-
-        else:
-            print(prt_success)
-
-    elif "-D" in args_array:
+    elif "-i" in args_array and "-D" in args_array:
         elr = create_es_instance(cfg, elastic_class.ElasticSearchRepo,
                                  args_array["-D"])
+        elr.connect()
+        dir_path = os.path.join(args_array["-P"], args_array["-D"], "indices")
 
-        if args_array["-D"] in elr.repo_dict:
-            print(prt_failure)
+        # Count number of databases/indices dumped to repository.
+        if len([name for name in os.listdir(dir_path)
+                if os.path.isdir(os.path.join(dir_path, name))]) >= 1:
+
+            print(prt_good)
 
         else:
-            print(prt_success)
+            print(prt_fail)
+
+        _ = remove_repo(elr, args_array["-D"], args_array["-P"])
+
+    elif "-D" in args_array:
+        els = create_es_instance(cfg, elastic_class.ElasticSearchDump,
+                                 args_array["-D"])
+        els.connect()
+        elr = create_es_instance(cfg, elastic_class.ElasticSearchRepo,
+                                 args_array["-D"])
+        elr.connect()
+
+        if els.dump_list:
+            print(prt_good)
+
+        else:
+            print(prt_fail)
+
+        _ = remove_repo(elr, args_array["-D"], args_array["-P"])
+
+    elif "-I" in args_array:
+        els = create_es_instance(cfg, elastic_class.ElasticSearchDump)
+        els.connect()
+
+        print(str(
+            [x.split() for x in els.els.cat.indices().splitlines()][0][2]))
 
 
 if __name__ == "__main__":
