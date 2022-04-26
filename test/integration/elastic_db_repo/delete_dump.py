58ccee6644557d1177e6f6069b8a5403d4797fef
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2022-04-26 16:08:17 +0100

    Update test/integration/elastic_db_repo/delete_dump.py v0.3.5

--- test/integration/elastic_db_repo/delete_dump.py
+++ test/integration/elastic_db_repo/delete_dump.py
@@ -73,10 +73,10 @@ class UnitTest(unittest.TestCase):
             self.cfg, "ssl_client_ca") else None
         self.scheme = self.cfg.scheme if hasattr(
             self.cfg, "scheme") else "https"
-        self.els = elastic_class.ElasticSearchRepo(
+        self.elr = elastic_class.ElasticSearchRepo(
             self.cfg.host, port=self.cfg.port, user=self.user, japd=self.japd,
             ca_cert=self.ca_cert, scheme=self.scheme)
-        self.els.connect()
+        self.elr.connect()
 
         if self.elr.repo_dict:
             print("ERROR: Test environment not clean - repositories exist.")
@@ -106,8 +106,8 @@ class UnitTest(unittest.TestCase):
 
         args_array = {"-r": self.repo_name, "-S": self.dump_name}
 
-        self.assertFalse(elastic_db_repo.delete_dump(self.elr,
-                                                     args_array=args_array))
+        self.assertFalse(elastic_db_repo.delete_dump(
+            self.elr, args_array=args_array))
 
     def test_deletedmp_arg(self):
 
