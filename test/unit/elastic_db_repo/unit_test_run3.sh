f0972768b71d1d5c9cfdb5fc0e0654e449c9f391
Mark Pernot <mark.pernot@gmail.com>
2022-12-22 10:10:13 +0000

    Update unit test modules v0.3.6

---
+++ test/unit/elastic_db_repo/unit_test_run3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Unit testing program for the elastic_db_repo.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit test"
+/usr/bin/python3 test/unit/elastic_db_repo/help_message.py
+/usr/bin/python3 test/unit/elastic_db_repo/list_dumps.py
+/usr/bin/python3 test/unit/elastic_db_repo/create_repo.py
+/usr/bin/python3 test/unit/elastic_db_repo/delete_repo.py
+/usr/bin/python3 test/unit/elastic_db_repo/delete_dump.py
+/usr/bin/python3 test/unit/elastic_db_repo/rename_repo.py
+/usr/bin/python3 test/unit/elastic_db_repo/rename.py
+/usr/bin/python3 test/unit/elastic_db_repo/disk_usage.py
+/usr/bin/python3 test/unit/elastic_db_repo/list_repos.py
+/usr/bin/python3 test/unit/elastic_db_repo/run_program.py
+/usr/bin/python3 test/unit/elastic_db_repo/main.py
+
